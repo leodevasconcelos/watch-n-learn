273f3b114dd945a43e350d026040120b7f0d1b56
andela-cganga <ganga.chris@gmail.com>
2016-02-06 14:26:38 +0300

    (chore) Use Auth in Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,13 @@
 
 namespace Learn\Http\Controllers;
 
+use Auth;
 use Learn\Http\Requests;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
