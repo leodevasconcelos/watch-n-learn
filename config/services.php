f5d9aa31955decf4bdd8fa4fb4feb319bb35e8a2
andela-cganga <ganga.chris@gmail.com>
2016-02-06 14:24:27 +0300

    [Feature #112750181] OAuth services configuration

--- config/services.php
+++ config/services.php
@@ -35,9 +35,28 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
-    'github' => [
-        'client_id' => env('GITHUB_ID'),
-        'client_secret' => env('GITHUB_SECRET'),
-        'redirect' => '',
-    ]
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_ID'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect' => env('FACEBOOK_URL')
+    ],
+
+    // 'github' => [
+    //     'client_id' => env('GITHUB_ID'),
+    //     'client_secret' => env('GITHUB_SECRET'),
+    //     'redirect' => evn('GITHUB_URL')
+    // ],
+
+    // 'google' => [
+    //     'client_id' => env('GOOGLE_ID'),
+    //     'client_secret' => env('GOOGLE_SECRET'),
+    //     'redirect' => evn('GOOGLE_URL')
+    // ],
+
+    'twitter' => [
+        'client_id' => env('TWITTER_ID'),
+        'client_secret' => env('TWITTER_SECRET'),
+        'redirect' => env('TWITTER_URL')
+    ],
 ];
