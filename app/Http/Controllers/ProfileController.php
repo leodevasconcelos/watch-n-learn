812c5bbbbe783b319aa16dbb57daa9b8e4067526
andela-cganga <ganga.chris@gmail.com>
2016-02-07 19:36:21 +0300

    [Feature #112750257] Add route functions to Profile Controllers

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,5 +9,38 @@ use Learn\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
