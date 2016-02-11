0f4f7a083e10a27f3007edff7a47daa662accce7
andela-cganga <ganga.chris@gmail.com>
2016-02-11 13:53:33 +0300

    [Feature #112750257] View for profile settings

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,31 +3,65 @@
   <div class="container row content">
     <div class="col s3">
       <div class="prof-image-div">
-        <img class="z-depth-1" src="http://placehold.it/250x250" alt="">
+        <img class="z-depth-1 profile-image" src="{{ $user->avatar }}" alt="">
       </div>
       <div class="details">
           <div>
-            <span class="username flow-text">gangachris</span>
+            <span class="username flow-text">{{ $user->username }}</span>
           </div>
           <div>
-            <span class="name flow-text">Ganga Christopher</span>
+            <span class="name flow-text">{{ $user->name }}</span>
           </div>
           <div>
-            <p class="bio flow-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, quas, quae! Quis sapiente quia ipsum error beatae culpa fuga minima qui, aut nisi. Inventore dolorum saepe amet vero dicta veniam.</p>
+            <p class="bio flow-text">{{ $user->bio }}</p>
           </div>
       </div>
     </div>
     <div class="col s9 row">
       <div class="col s12">
         <ul class="tabs">
-          <li class="tab col s3"><a href="#projects" class="active">Projects</a></li>
+          <li class="tab col s3"><a href="#projects">Projects</a></li>
           <li class="tab col s3"><a href="#upload">Upload New Project</a></li>
-          <li class="tab col s3"><a href="#settings">Profile Settings</a></li>
+          <li class="tab col s3"><a href="#settings" class="active">Profile Settings</a></li>
         </ul>
       </div>
       <div id="projects" class="col s12">Projects</div>
       <div id="upload" class="col s12">Upload a project</div>
-      <div id="settings" class="col s12">Profile Settings</div>
+      <div id="settings" class="col s12">
+        <div class="tab-content">
+          <h3 class="flow-text">Update Your Profile</h3>
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="full_name" type="text" class="validate" value = "{{ $user->name }}" required>
+                <label for="first_name">Full Name</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="username" type="text" class="validate" value="{{ $user->username }}">
+                <label for="username">Username</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" value="{{ $user->email }}" required>
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="bio" class="materialize-textarea">{{$user->bio}}</textarea>
+                <label for="bio">Bio</label>
+              </div>
+            </div>
+            <div class="row"><!--
+              <button class="waves-effect waves-light btn col s2 offset-s8"><i class="material-icons left">cancel</i>Cancel</a> -->
+              <button type="submit" class="waves-effect waves-light btn col s2 offset-s10"><i class="material-icons left">save</i>Save</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 @stop
