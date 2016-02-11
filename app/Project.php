ab9fece1b57add61f1df1d3530d383324a8807f3
andela-cganga <ganga.chris@gmail.com>
2016-02-11 19:01:02 +0300

    [Feature #112750275] Add Project Model, with mass asignable fillables

---
+++ app/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Learn;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'category', 'url', 'description', 'user_id'
+    ];
+}
