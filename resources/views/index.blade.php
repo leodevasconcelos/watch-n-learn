64be84500e4d40764010a0dfec949600ea0e464d
andela-cganga <ganga.chris@gmail.com>
2016-02-13 15:11:39 +0300

    [Feature #112750217] Add parallax for landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Hello </h1>
+    <div class="parallax-container">
+    <div class="parallax">
+        <img src="/images/desk.jpeg">
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eos eaque atque voluptatum voluptatem cumque beatae, facilis doloremque mollitia reiciendis nulla harum ipsa officia temporibus odio cum tempora quos modi!</h1>
+    </div>
+  </div>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">Parallax</h2>
+      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+    </div>
+  </div>
 @endsection
