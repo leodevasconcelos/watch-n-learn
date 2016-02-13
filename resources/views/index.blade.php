e3a5456b61689250abf05dd4063cbe7a19175f2f
andela-cganga <ganga.chris@gmail.com>
2016-02-13 15:38:55 +0300

    [Feature #112750217] Display learning resources in landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,17 @@
   </div>
   <div class="section white">
     <div class="row container">
-      <h2 class="header">Parallax</h2>
-      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      <h2 class="flow-text">Go from Zero to One, by watching n learning</h2>
+       <div class="row">
+            @foreach($projects as $project)
+            <div class="col s3">
+                <div class="project">
+                    <iframe width="250" height="141" src="https://www.youtube.com/embed/{{ $project->url }}" frameborder="0" allowfullscreen></iframe>
+                    <h6 class="flow-text"><a href="#"> {{$project->title}} </a></h6>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
   </div>
 @endsection
