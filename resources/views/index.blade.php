134bfb7dcae7c8a60f5825b716e168d23a968d58
andela-cganga <ganga.chris@gmail.com>
2016-02-14 15:59:25 +0300

    (chore) Remove extra code from index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
     <div class="parallax-container">
-    <div class="parallax">
-        <img src="/images/desk.jpeg">
-        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eos eaque atque voluptatum voluptatem cumque beatae, facilis doloremque mollitia reiciendis nulla harum ipsa officia temporibus odio cum tempora quos modi!</h1>
+        <div class="parallax">
+            <img src="/images/desk.jpeg">
+        </div>
     </div>
-  </div>
-  <div class="section white">
-    <div class="row container">
-      <h2 class="flow-text">Go from Zero to One, by watching n learning</h2>
-       <div class="row">
+    <div class="section white">
+        <div class="row container">
+            <h2 class="flow-text">Go from Zero to One, by watching n learning</h2>
+            <div class="row">
             @foreach($projects as $project)
-            <div class="col s3">
-                <div class="project">
-                    <iframe width="250" height="141" src="https://www.youtube.com/embed/{{ $project->url }}" frameborder="0" allowfullscreen></iframe>
-                    <h6 class="flow-text"><a href="#"> {{$project->title}} </a></h6>
+                <div class="col s3">
+                    <div class="project">
+                        <iframe width="250" height="141" src="https://www.youtube.com/embed/{{ $project->url }}" frameborder="0" allowfullscreen></iframe>
+                        <h6 class="flow-text"><a href="{{ url('projects/'.$project->id) }}"> {{$project->title}} </a></h6>
+                    </div>
                 </div>
-            </div>
             @endforeach
         </div>
     </div>
