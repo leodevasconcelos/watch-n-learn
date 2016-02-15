a4dddd2bcdc515a8bb444eec4b3108d2389df57a
andela-cganga <ganga.chris@gmail.com>
2016-02-15 21:45:15 +0300

    [Feature #112750437] Add hasMany comments relationship to Project model

--- app/Project.php
+++ app/Project.php
@@ -14,4 +14,9 @@ class Project extends Model
     protected $fillable = [
         'title', 'category', 'url', 'description', 'user_id',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany('Learn\Comment');
+    }
 }
