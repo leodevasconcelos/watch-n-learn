3a8f9688ab8d69399b74afb91056e4fbe01615a3
andela-cganga <ganga.chris@gmail.com>
2016-02-15 21:39:26 +0300

    [Feature #112750437] Handle ajax post for comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,25 @@
 $(document).ready(function () {
   $('select').material_select()
   $('.parallax').parallax()
+  $('#comment').keypress(function (key) {
+    if (key.keyCode === 13) {
+      var data = {
+        comment: $(this).val(),
+        _token: $('#_token').val(),
+        project_id: $(this).attr('data-id')
+      }
+
+      $(this).val('')
+
+      $.post('/projects/comment', data, function (res) {
+        var html = ''
+        html += '<div class="comment-view z-depth-1">'
+        html += '<h5>' + res.name + '</h5>'
+        html += '<p>' + res.comment + '</p>'
+        html += '</div>'
+
+        $('.comments-list').append(html)
+      })
+    }
+  })
 })
