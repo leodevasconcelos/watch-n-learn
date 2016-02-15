993223626d742d90008146dd5cc1b0e020362965
andela-cganga <ganga.chris@gmail.com>
2016-02-15 22:06:14 +0300

    [Feature #112750437] Display available commets on project page load

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -11,12 +11,17 @@
                     <h1 class="flow-text">{{ $project->title }}</h1>
                 </div>
                 <h1 class="flow-text col s1">23  <i class="small material-icons">thumb_up</i></h1>
-                <h1 class="flow-text col s1">23 <i class="small material-icons">chat_bubble_outline</i></h1>
+                <h1 class="flow-text col s1"><span id="commentCount">{{ count($comments) }} </span><i class="small material-icons">chat_bubble_outline</i></h1>
             </div>
             <div class="comments">
                 <h2 class="flow-text">Comment below</h2>
                 <div class="comments-list">
-
+                    @foreach($comments as $comment)
+                    <div class="comment-view z-depth-1">
+                    <h5>{{ $comment->user->name }}</h5>
+                    <p>{{ $comment->comment }}</p>
+                    </div>
+                    @endforeach
                 </div>
                 <div class="input-field col s12 comment">
                     <input type="hidden" name="_token" id="_token" value="{{ csrf_token() }}">
