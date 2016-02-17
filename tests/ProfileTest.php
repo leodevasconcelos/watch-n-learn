872419b7463765682cab578257d565c62c70016f
andela-cganga <ganga.chris@gmail.com>
2016-02-17 11:58:54 +0300

    [Chore #112980615] Test update profile

---
+++ tests/ProfileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProfileTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testProfileUpdate()
+    {
+        $user = factory(Learn\User::class)->create();
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->see('Update Your Profile')
+            ->type('Ganga Christopher', 'name')
+            ->type('gangachris', 'username')
+            ->press('Save')
+            ->seeInDatabase('users', ['username' => 'gangachris']);
+    }
+}
