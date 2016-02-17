c160441d2262bf47417bab00d3c806d429a48e41
andela-cganga <ganga.chris@gmail.com>
2016-02-17 10:10:51 +0300

    [Chore #112980615] Test for login

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -7,8 +7,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class AuthTest extends TestCase
 {
     use DatabaseMigrations;
+
     /**
-     * A basic test example.
+     * Test Regisering a new user
      *
      * @return void
      */
@@ -22,6 +23,27 @@ class AuthTest extends TestCase
             ->type('123456789', 'password_confirmation')
             ->press('Register')
             ->seePageIs('/')
+            ->see('Ganga Christopher')
             ->seeInDatabase('users', ['email' => 'ganga.chris@gmail.com']);
+    }
+
+    /**
+     * Test Login a new user
+     */
+    public function testLoginUser()
+    {
+        $this->visit('/register')
+            ->type('Ganga Christopher', 'name')
+            ->type('gangachris', 'username')
+            ->type('ganga.chris@gmail.com', 'email')
+            ->type('123456789', 'password')
+            ->type('123456789', 'password_confirmation')
+            ->press('Register')
+            ->visit('/logout')
+            ->visit('/login')
+            ->type('ganga.chris@gmail.com', 'email')
+            ->type('123456789', 'password')
+            ->press('Login')
+            ->see('Ganga Christopher');
     }
 }
