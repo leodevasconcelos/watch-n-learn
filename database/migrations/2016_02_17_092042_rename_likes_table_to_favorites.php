2a9d2226405b24687d8d5962f6dec51870c5aade
andela-cganga <ganga.chris@gmail.com>
2016-02-17 12:28:21 +0300

    [Fixes #113905849] Rename Likes table migration

---
+++ database/migrations/2016_02_17_092042_rename_likes_table_to_favorites.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameLikesTableToFavorites extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('likes', 'favorites');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::rename('favorites', 'likes');
+    }
+}
