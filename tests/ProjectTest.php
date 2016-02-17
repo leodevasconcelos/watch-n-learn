fa21cb975f6d0737a1c3455f9ed1c2c4442c6d1a
andela-cganga <ganga.chris@gmail.com>
2016-02-17 11:11:02 +0300

    [Chore #112980615] Test Creation of Project

---
+++ tests/ProjectTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProjectTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateProject()
+    {
+        $user = factory(Learn\User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/dashboard')
+            ->type('Sorry, Justin Beiber', 'title')
+            ->select('other', 'category')
+            ->type('https://www.youtube.com/watch?v=fRh_vgS2dFE', 'url')
+            ->type('This is JB\'s song Sorry', 'description')
+            ->press('Upload')
+            ->seeInDatabase('projects', ['url' => 'fRh_vgS2dFE']);
+    }
+}
