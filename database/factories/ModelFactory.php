a60e2528164ed4703622286dc43c1c9d42b7d4cd
andela-cganga <ganga.chris@gmail.com>
2016-02-17 11:54:57 +0300

    (chore) Add Project factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -20,3 +20,13 @@ $factory->define(Learn\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(Learn\Project::class, function (Faker\Generator $faker) {
+    return [
+        'title'       => $faker->city,
+        'youtubeID'   => $faker->randomElement(['yxeKGzW-BFs', '7TF00hJI78Y', 'gqOEoUR5RHg', 'fju9ii8YsGs', 'gqOEoUR5RHg']),
+        'user_id'     => $faker->numberBetween(1, 20),
+        'category'    => "Programming",
+        'description' => $faker->sentence(40),
+    ];
+});
