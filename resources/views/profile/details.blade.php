c170e2f35cccc13ebc47095061a5ff56aca6ddcc
andela-cganga <ganga.chris@gmail.com>
2016-02-19 11:42:26 +0300

    [Feature #112750257] View for updating profile pic

--- resources/views/profile/details.blade.php
+++ resources/views/profile/details.blade.php
@@ -1,6 +1,13 @@
 <div class="col s3">
   <div class="prof-image-div">
     <img class="z-depth-1 profile-image" src="{{ $user->getAvatar() }}" alt="">
+    @if(Auth::user()->id == $user->id)
+    <form id="profileImageForm" name="profileImageForm" method="POST" action="{{ url('/dashboard/updatePic') }}" enctype="multipart/form-data">
+        {!! csrf_field() !!}
+        <input type="file" name="image" id="image" style="display:none;">
+    </form>
+    <a href="#" id="changeProfPic">Change you profile pic</a>
+    @endif
   </div>
   <div class="details">
       <div>
