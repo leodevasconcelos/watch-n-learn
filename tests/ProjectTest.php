3bd6b624932174ba9710040bb1e04d7b5de017c3
andela-cganga <ganga.chris@gmail.com>
2016-02-20 13:25:44 +0300

    [Chore #112980615] Test for correct PRoject shown when url is clicked

--- tests/ProjectTest.php
+++ tests/ProjectTest.php
@@ -24,4 +24,14 @@ class ProjectTest extends TestCase
             ->press('Upload')
             ->seeInDatabase('projects', ['url' => 'fRh_vgS2dFE']);
     }
+
+    public function testViewCorrectProject()
+    {
+        $project = factory(Learn\Project::class)->create();
+        $user = factory(Learn\User::class)->create();
+        $this->visit('/')
+            ->see($project->title)
+            ->visit('/projects/'.$project->id)
+            ->see($project->title);
+    }
 }
