9782d9877b3de8e807fb24b329ecae00b295a49d
andela-cganga <ganga.chris@gmail.com>
2016-02-21 15:21:40 +0300

    [Chore #112980615] Test Project update

--- tests/ProjectTest.php
+++ tests/ProjectTest.php
@@ -16,13 +16,13 @@ class ProjectTest extends TestCase
         $user = factory(Learn\User::class)->create();
 
         $this->actingAs($user)
-            ->visit('/dashboard')
-            ->type('Sorry, Justin Beiber', 'title')
-            ->select('other', 'category')
-            ->type('https://www.youtube.com/watch?v=fRh_vgS2dFE', 'url')
-            ->type('This is JB\'s song Sorry', 'description')
-            ->press('Upload')
-            ->seeInDatabase('projects', ['url' => 'fRh_vgS2dFE']);
+        ->visit('/dashboard')
+        ->type('Sorry, Justin Beiber', 'title')
+        ->select('other', 'category')
+        ->type('https://www.youtube.com/watch?v=fRh_vgS2dFE', 'url')
+        ->type('This is JB\'s song Sorry', 'description')
+        ->press('Upload')
+        ->seeInDatabase('projects', ['url' => 'fRh_vgS2dFE']);
     }
 
     public function testViewCorrectProject()
@@ -30,14 +30,38 @@ class ProjectTest extends TestCase
         $project = factory(Learn\Project::class)->create();
         $user = factory(Learn\User::class)->create();
         $this->visit('/')
-            ->see($project->title)
-            ->visit('/projects/'.$project->id)
-            ->see($project->title);
+        ->see($project->title)
+        ->visit('/projects/'.$project->id)
+        ->see($project->title);
     }
 
     public function testVisitEditWithoutAuth()
     {
         $this->visit('/projects/1/edit')
-            ->seePageIs('/login');
+        ->seePageIs('/login');
+    }
+
+    public function testEditProject()
+    {
+        $title = 'This is a test title';
+        $description = 'This ia a test description';
+        $category = 'Programming';
+        $url = 'https://www.youtube.com/watch?v=fRh_vgS2dFE';
+
+        $project = factory(Learn\Project::class)->create();
+        $user = factory(Learn\User::class)->create();
+        $response = $this->call(
+            'PUT',
+            'projects/'.$project->id,
+            [
+                'title'       => $title,
+                'description' => $description,
+                'category'    => $category,
+                'url'         => $url,
+                '_token'      => csrf_token(),
+            ]
+        );
+
+        $this->seeInDatabase('projects', ['title' => $title, 'description' => $description]);
     }
 }
