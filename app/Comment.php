2df7cbc4d90e502dc5681de9338654a858147494
andela-cganga <ganga.chris@gmail.com>
2016-02-23 09:16:32 +0300

    (chore) Comment on Models

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * Comment belongs to User relationship
+     */
     public function user()
     {
         return $this->belongsTo('Learn\User');
--- app/Favorite.php
+++ app/Favorite.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+    /**
+     * Favorite belongs to User relationship
+     */
     public function user()
     {
         return $this->belongsTo('Learn\User');
     }
 
+    /**
+     * Favorite belongs to a project relationship
+     */
     public function project()
     {
         return $this->belongsTo('Learn\Project');
