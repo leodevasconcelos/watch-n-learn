85ce8d2e2d023829278866405e2964ec9faf1fe6
andela-cganga <ganga.chris@gmail.com>
2016-02-24 17:51:18 +0300

    [Fixes #114340247] Add youtube url validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Learn\Providers;
 
+use Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('youtube', function ($attribute, $value, $parameters, $validator) {
+            return [$value] == preg_grep("%^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/watch\?v=([^&]+)%", [$value]);
+        }, 'Invalid youtube video url');
     }
 
     /**
